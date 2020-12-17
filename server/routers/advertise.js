b034bc2c38b9f5f467522f765eb1a01365ddf84c
abtahi-tajwar <abtahitajwar@gmail.com>
2020-12-17 23:24:52 +0600

    Abtahi Tajwar: Add advertise Product, Remove Advertise Product

--- server/routers/advertise.js
+++ server/routers/advertise.js
@@ -18,6 +18,17 @@ const advertisePicture = multer({
         callback(undefined, true)
     }
 })
+const productPicture = multer({
+    limit: {
+        fileSize: 5000000 
+    }, 
+    fileFilter(req, file, callback) {
+        if(!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+            callback(new Error('Only upload jpg/jpeg/png file'))
+        }
+        callback(undefined, true)
+    }
+})
 
 advertiseRouter.post('/advertise/create', auth, async (req, res) => {
     if(req.user.type !== process.env.USER_TYPE_SELLER) {
@@ -27,7 +38,7 @@ advertiseRouter.post('/advertise/create', auth, async (req, res) => {
     advertise.author = req.user._id
     try {
         await advertise.save()
-        res.status(200).send(advertise)
+        res.status(201).send(advertise)
     } catch (e) {
         res.status(500).send()
     }
@@ -38,7 +49,7 @@ advertiseRouter.delete('/advertise/:id', auth, async (req, res) => {
         if(!ad) {
             return res.status(404).send('No matching ad found')
         }
-        ad.remove()
+        await ad.remove()
         res.status(202).send(ad)
     } catch (e) {
         res.status(500).send()
@@ -61,8 +72,17 @@ advertiseRouter.patch('/advertise/:id', auth, async (req, res) => {
         return res.status(400).send('Some updates are invalid')
     }
     try {
-        const ad = await Advertise.findById(req.params.id)
-        changes.forEach(change => ad[change] = req.body[change])
+        const ad = await Advertise.findOne({ _id: req.params.id, author: req.user._id })
+        changes.forEach(change => {
+            if(change === 'location') {
+                locationChanges.forEach(item => {
+                    ad.location[item] = req.body.location[item]
+                })
+            } else {
+                ad[change] = req.body[change]
+            }
+        })
+        await ad.save()
         res.status(200).send(ad)
     } catch (e) {
         res.status(500).send()
@@ -83,7 +103,7 @@ advertiseRouter.post('/advertise/picture/:id', auth, advertisePicture.single('ad
 })
 advertiseRouter.delete('/advertise/picture/:id/:serial', auth, async (req, res) => {
     try {
-        const ad = await findOne({ _id: req.params.id, author: user.id })
+        const ad = await findOne({ _id: req.params.id, author: req.user.id })
         if(!ad) {
             res.status(404).send('Advertise not found')
         }
@@ -91,9 +111,34 @@ advertiseRouter.delete('/advertise/picture/:id/:serial', auth, async (req, res) 
         await ad.save()  
 
     } catch (e) {
-        req.status(500).send()
+        res.status(500).send()
+    }   
+    
+})
+advertiseRouter.post('/advertise/product/:id', auth, productPicture.single('productPicture'), async (req, res) => {
+    try {
+        const buffer = await sharp(req.file.buffer).resize({ height: 500, width: 500 }).jpeg().toBuffer()
+        const ad = await Advertise.findOne({ _id: req.params.id, author: req.user._id })
+        if(!ad) {
+            return res.status(404).send('Advertise Not Found')
+        }        
+        ad.products.push({ name: req.body.name, image: buffer })
+        await ad.save()
+        res.status(200).send(ad) 
+    } catch (e) {
+        res.status(500).send()
     }
-    
-    
+})
+advertiseRouter.delete('/advertise/product/:id', auth, async (req, res) => {
+    try {
+        const ad = await Advertise.findOne({ _id: req.params.id, author: req.user._id })
+        if(!ad) {
+            return res.status(404).send('Advertise Not Found')
+        }
+        await ad.remove()
+        res.status(202).send(ad)
+    } catch (e) {
+        res.status(500).send()
+    }
 })
 module.exports = advertiseRouter
